03aee5c852b88a71c6cffbe191b12bed2e5cc497
ArvinN15 <145598643+ArvinN15@users.noreply.github.com>
2023-11-17 16:36:53 -0500

    Add files via upload

--- Assignment 3/public/index.js
+++ Assignment 3/public/index.js
@@ -1,45 +1,61 @@
 // index.js
 
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Assignment = require('./models/assignment');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set the views directory to 'views'
-app.set('views', path.join(__dirname, 'views'));
+mongoose.connect('mongodb+srv://arvinnaresh:<password>@cluster0.kswhu5z.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Serve static files from the 'views' directory
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// ... (other configurations)
+// Serve your static files
+app.use(express.static('public'));
 
-app.get('/public', (req, res) => {
-  console.log('Accessed /public route');
-  // Render the HTML file directly
-  res.sendFile(path.join(__dirname, 'views', 'publicPage.html'));
+// Your existing routes
+
+// Route to get all assignments
+app.get('/assignments', async (req, res) => {
+  try {
+    const assignments = await Assignment.find();
+    res.render('publicPage', { assignments });
+  } catch (error) {
+    console.error('Error fetching assignments:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Express route to add an assignment
+// Route to add a new assignment
 app.post('/add-assignment', async (req, res) => {
   const { title, description } = req.body;
 
   try {
-    // Create a new assignment in the database
     const newAssignment = await Assignment.create({ title, description });
-
-    // Retrieve all assignments from the database
-    const assignments = await Assignment.find();
-
-    // Send the assignments back to the client
-    res.render('publicPage', { assignments });
+    res.json(newAssignment);
   } catch (error) {
     console.error('Error adding assignment:', error);
-    res.status(500).json({ error: 'Failed to add assignment' });
+    res.status(500).send('Internal Server Error');
   }
 });
 
-// ... (remaining code)
+// Route to delete an assignment
+app.post('/delete-assignment/:id', async (req, res) => {
+  const assignmentId = req.params.id;
+
+  try {
+    const deletedAssignment = await Assignment.findByIdAndRemove(assignmentId);
+    res.json(deletedAssignment);
+  } catch (error) {
+    console.error('Error deleting assignment:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Other CRUD routes (update, edit) can be added similarly
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
